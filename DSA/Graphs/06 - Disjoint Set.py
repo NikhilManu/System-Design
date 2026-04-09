df35dfb640cba9803b0a898e6b219c8bdb49ba6a
Nikhil Manu <nikhilsmanu@gmail.com>
2026-04-09 18:37:49 +0530

    DSU algorithm

---
+++ DSA/Graphs/06 - Disjoint Set.py
@@ -0,0 +1,33 @@
+class DSU:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * (n + 1)
+        self.size = [1] * (n + 1)
+
+    def findParent(self, n):
+        if self.parent[n] == n:
+            return n
+
+        self.parent[n] = self.findParent(self.parent[n])
+        return self.parent[n]
+    
+    def unionBySize(self, n1, n2):
+        p1, p2 = self.findParent(n1), self.findParent(n2)
+
+        if self.size[p1] < self.size[p2]:
+            self.parent[p1] = p2
+            self.size[p2] += self.size[p1]
+        else:
+            self.parent[p2] = p1
+            self.size[p1] += self.size[p2]
+
+    def unionByRank(self, n1, n2):
+        p1, p2 = self.findParent(n1), self.findParent(n2)
+
+        if self.rank[p1] < self.rank[p2]:
+            self.parent[p1] = p2
+        elif self.rank[p2] < self.rank[p1]:
+            self.parent[p2] = p1 
+        else:
+            self.parent[p2] = p1
+            self.rank[p1] += 1
